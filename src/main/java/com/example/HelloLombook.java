bfa3967ee4812082c1e16f02091561932fc1f16c
HanJeongmok <wjdahe1845@gmail.com>
2026-03-08 23:11:52 +0900

    1강연습 종료

---
+++ src/main/java/com/example/HelloLombook.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class HelloLombook {
+    private final String hello;
+    private final int lombok;
+    public static void main(String[]args){
+        HelloLombook helloLombook = new HelloLombook("헬로",5);
+        System.out.println(helloLombook.getHello());
+        System.out.println(helloLombook.getLombok());
+    }
+}
