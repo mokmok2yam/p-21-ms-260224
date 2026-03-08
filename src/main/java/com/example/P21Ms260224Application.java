92630a84b95eb5f2b1b8a3e46a4a6950565a17c1
HanJeongmok <wjdahe1845@gmail.com>
2026-03-08 22:44:48 +0900

    first commit

---
+++ src/main/java/com/example/P21Ms260224Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P21Ms260224Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P21Ms260224Application.class, args);
+	}
+
+}
