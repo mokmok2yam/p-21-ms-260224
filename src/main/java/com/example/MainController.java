d3635e4d55d0e66cc8e68cf8ec9404c2ed5bf267
HanJeongmok <wjdahe1845@gmail.com>
2026-03-09 17:44:55 +0900

    2-02강 간단한 웹 프로그램 만들기

---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class MainController {
+    @GetMapping("/sbb")
+    public String index(){
+        return "안녕하세요 sbb에 오신 것을 환영합니다.";
+    }
+}
