0f9fb6b70fb0de7f8af426008ff426e77bf17b47
HanJeongmok <wjdahe1845@gmail.com>
2026-03-10 21:46:14 +0900

    2-04테이블로 메핑하기

---
+++ src/main/java/com/example/Answer.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(columnDefinition = "text")
+    private String content;
+    @ManyToOne
+    private Question question;
+    private LocalDateTime createDate;
+}
