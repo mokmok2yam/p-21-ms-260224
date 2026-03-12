6635df69d63f2f8af388e30fe61051e761965efd
HanJeongmok <wjdahe1845@gmail.com>
2026-03-12 22:30:49 +0900

    2-11 URL 프리픽스 알아 두기

--- src/main/java/com/example/question/QuestionController.java
+++ src/main/java/com/example/question/QuestionController.java
@@ -4,22 +4,33 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/question")
 @RequiredArgsConstructor
 public class QuestionController {
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    //글 목록 조회
+    @GetMapping("/list")
     public String  list(Model model){
         List<Question> questionList = questionService.getList();
         model.addAttribute("questionList",questionList);
         return "question_list";
     }
-    @GetMapping("/")
-    public String root(){
-        return "redirect:/question/list";
+    //상세보기
+    @GetMapping("/detail/{id}")
+    public String detail(
+            @PathVariable int id,
+            Model model
+    ){
+        Question question = questionService.getQuestion(id);
+        model.addAttribute("question",question);
+        return "question_detail";
+
     }
 }
--- src/main/java/com/example/question/QuestionService.java
+++ src/main/java/com/example/question/QuestionService.java
@@ -1,15 +1,28 @@
 package com.example.question;
 
+import com.example.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class QuestionService {
     private final QuestionRepository questionRepository;
+    //다건 조회
     public List<Question> getList(){
         return questionRepository.findAll();
     }
+    //단건 조회
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        }else{
+            throw new DataNotFoundException("question not found");
+        }
+    }
+
 }
